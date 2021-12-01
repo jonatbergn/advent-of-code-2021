0be21875d8421d41226d5fda7b9a229ae1aa5351
Jonathan Bergen <mail@jonathanbergen.com>
2021-12-01 08:56:55 +0100

    Day01_part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,12 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input
-            .map(String::toInt)
-            .zipWithNext()
-            .count { it.first < it.second }
-    }
+    fun part1(input: List<Int>) = input.zipWithNext().count { it.first < it.second }
+    fun part2(input: List<Int>) = part1(input.windowed(size = 3, partialWindows = false) { it.sum() })
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInputAsInts("Day01_test")
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInputAsInts("Day01")
     println(part1(input))
     println(part2(input))
 }
